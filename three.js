6c5dcd85d2432c4c93e2e2b1b1dd123c096da52c
Alireza Esmaeili <alirezaesmaeili.cs@gmail.com>
2025-07-13 10:18:44 +0330

    delet items with Event Delegation

--- three.js
+++ three.js
@@ -1,10 +1,9 @@
 const addButton = document.getElementById('add-item');
 const itemList = document.getElementById('item-list');
 
-// شمارنده آیتم‌ها
+
 let itemCount = itemList.children.length;
 
-// افزودن آیتم جدید
 addButton.addEventListener('click', () => {
   itemCount++;
   
@@ -14,7 +13,6 @@ addButton.addEventListener('click', () => {
   itemList.appendChild(li);
 });
 
-// حذف آیتم با Event Delegation
 itemList.addEventListener('click', (event) => {
   if (event.target.classList.contains('delete-btn')) {
     const li = event.target.closest('li');
